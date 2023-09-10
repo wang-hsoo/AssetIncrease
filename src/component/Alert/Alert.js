82d74f51a887c2b074bd608e3e20d9cacefa2be5
wang-hsoo <dhkd4873@gamil.com>
2023-09-10 13:15:29 +0900

    assetIncrease v1.0 - pwa 기능 추가

--- src/component/Alert/Alert.js
+++ src/component/Alert/Alert.js
@@ -114,10 +114,10 @@ function Alert(){
             <Container>
                 <Box onOff={onOff}>
                     <div><Download_icon />다운로드 하시겠습니까?</div>
-                    {/* <BTN>
+                    <BTN>
                         <div onClick={() => Install()}>확인</div>
                         <div onClick={()=> cancel()}>취소</div>
-                    </BTN> */}
+                    </BTN>
                 </Box>
             </Container>
         </Layout>
